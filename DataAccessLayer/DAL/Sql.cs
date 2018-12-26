0ddee9295cb97e26dc1e91a43e9048f6ad148f0b
usman <usman.solemn@gmail.com>
2018-12-26 22:53:09 +0500

    DAL introduced for layered architecture, clean and refactored code.

---
+++ DataAccessLayer/DAL/Sql.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class Sql
+    {
+        public static List<string> FetchRecords()
+        {
+            var tempRecords = new List<string>();
+            var connectionString = "Data Source=localhost;Initial Catalog=techintalkDb;User Id=sa;Password=root;";
+            var query = "select * from users";
+            var sqlConnection = new SqlConnection(connectionString);
+            var command = new SqlCommand(query, sqlConnection);
+            SqlDataReader reader = null;
+
+            try
+            {
+                sqlConnection.Open();
+                reader = command.ExecuteReader();
+                
+                while (reader.Read())
+                {
+                    tempRecords.Add("User id is : " + reader.GetValue(0) + " and its name is : " + reader.GetValue(1));
+                }
+            }
+            catch(Exception exp)
+            {
+
+            }
+            sqlConnection.Close();
+            command.Dispose();
+            reader.Close();
+            return tempRecords;
+        }
+    }
+}
--- SqlServerAdoSession/SqlConAdo/Form1.cs
+++ SqlServerAdoSession/SqlConAdo/Form1.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DAL;
+using System;
 using System.Windows.Forms;
 
 namespace SqlConAdo
@@ -20,19 +13,10 @@ namespace SqlConAdo
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string conStr = "Data Source=localhost;Initial Catalog=techintalkDb;User Id=sa;Password=root;";
-            string query = "select * from users";
-            SqlConnection con = new SqlConnection(conStr);
-
-            SqlCommand comm = new SqlCommand(query, con);
-
-            con.Open();
-
-            SqlDataReader reader = comm.ExecuteReader();
-
-            while (reader.Read())
+            var records = Sql.FetchRecords();
+            foreach (var record in records)
             {
-                MessageBox.Show("User id is : "+ reader.GetValue(0)+" and its name is : "+ reader.GetValue(1));
+                MessageBox.Show(record);
             }            
         }
     }
