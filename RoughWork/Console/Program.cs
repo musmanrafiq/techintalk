e31f16bfc73f2ce07ccbd829004bbc2463cfd55f
usman <usman.solemn@gmail.com>
2019-01-05 19:37:18 +0500

    Just ignore this, its my rought work :p

---
+++ RoughWork/Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+            CancellationToken token = tokenSource.Token;
+
+            Task.Run(() =>
+            {
+                while (!tokenSource.IsCancellationRequested)
+                {
+                    token.ThrowIfCancellationRequested(); 
+                    Console.WriteLine("Hello World!");
+                    Thread.Sleep(200);
+                }
+            }, token);
+            tokenSource.Cancel();
+
+            Console.ReadKey();
+        }
+    }
+}
