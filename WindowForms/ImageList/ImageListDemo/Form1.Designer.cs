88b717777a9545d0f1ce41931fa1b89835bc444a
usman <usman.solemn@gmail.com>
2019-10-19 00:02:49 +0500

    Updates

---
+++ WindowForms/ImageList/ImageListDemo/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ImageListDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imageList = new System.Windows.Forms.ListView();
+            this.selectedImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.selectedImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList
+            // 
+            this.imageList.HideSelection = false;
+            this.imageList.Location = new System.Drawing.Point(12, 12);
+            this.imageList.Name = "imageList";
+            this.imageList.Size = new System.Drawing.Size(170, 426);
+            this.imageList.TabIndex = 0;
+            this.imageList.UseCompatibleStateImageBehavior = false;
+            // 
+            // selectedImage
+            // 
+            this.selectedImage.Location = new System.Drawing.Point(198, 12);
+            this.selectedImage.Name = "selectedImage";
+            this.selectedImage.Size = new System.Drawing.Size(453, 388);
+            this.selectedImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.selectedImage.TabIndex = 1;
+            this.selectedImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 450);
+            this.Controls.Add(this.selectedImage);
+            this.Controls.Add(this.imageList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.selectedImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView imageList;
+        private System.Windows.Forms.PictureBox selectedImage;
+    }
+}
+
---
+++ WindowForms/ImageList/ImageListDemo/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ImageListDemo
+{
+    public partial class Form1 : Form
+    {
+        private List<Image> LoadedImages { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // loading images from folder
+            LoadImagesFromFolder();
+
+            // initializing images list
+            ImageList images = new ImageList();
+            images.ImageSize = new Size(130, 40);
+
+
+            foreach(var image in LoadedImages)
+            {
+                images.Images.Add(image);
+            }
+
+            // setting our listview with the imagelist
+            imageList.LargeImageList = images;
+
+            for (int itemIndex = 1; itemIndex <= LoadedImages.Count; itemIndex++)
+            {
+                imageList.Items.Add(new ListViewItem($"Image {itemIndex}", itemIndex-1));
+            }
+        }
+
+        private void LoadImagesFromFolder()
+        {
+            LoadedImages = new List<Image>();
+            var index = 1;
+            while(index < 10)
+            {
+                string tempLocation = $@"E:\temp\images\{index}.png";
+                var tempImage = Image.FromFile(tempLocation);
+                LoadedImages.Add(tempImage);
+                index += 1;
+            }
+        }
+    }
+}
