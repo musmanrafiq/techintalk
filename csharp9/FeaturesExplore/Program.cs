57666ae19fab54a17d32ab183c416ec5115282db
musmanrafiq <usman.solemn@gmail.com>
2020-12-13 10:59:55 +0500

    updates

---
+++ csharp9/FeaturesExplore/Program.cs
@@ -0,0 +1,19 @@
+﻿using FeaturesExplore.Records;
+using System;
+
+namespace FeaturesExplore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee e = new Employee("e")
+            {
+                FullName = "test"
+            };
+            Employee ee = e with { FullName = "T" };
+            Console.WriteLine(ee.FullName);
+        }
+    }
+}
---
+++ csharp9/FeaturesExplore/Records/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeaturesExplore.Records
+{
+    public record Employee
+    {
+        public string FullName { get; set; }
+        public Employee(string name) => (FullName) = (name);
+    }
+}
