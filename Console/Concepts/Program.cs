ddd578081a1463ae498ec598643b4de0f6c86551
musmanrafiq <usman.solemn@gmail.com>
2020-12-28 14:42:08 +0500

    updates

--- Console/Concepts/Program.cs
+++ Console/Concepts/Program.cs
@@ -1,5 +1,8 @@
-﻿using Concepts.WhenKeyword;
+﻿using Concepts.SerializationDeserialization;
 using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Concepts
 {
@@ -10,9 +13,22 @@ namespace Concepts
             // in catch block
             // in swith statements
             // swith expression
-            var example1 = new WhenExample1();
-            var returnValue = example1.ProcessInput(9);
-            Console.WriteLine(returnValue);
+            //var example1 = new WhenExample1();
+            //var returnValue = example1.ProcessInput(9);
+            //Console.WriteLine(returnValue);
+
+            // serialization
+            var userModel = new UserModel { Id = 1, Name = "Usman" };
+            IFormatter formatter = new BinaryFormatter();
+            var stream = new FileStream("userinfo.txt", FileMode.Create, FileAccess.Write);
+            formatter.Serialize(stream, userModel);
+            stream.Close();
+
+            var readStream = new FileStream("userinfo.txt", FileMode.Open, FileAccess.Read);
+            UserModel model = (UserModel)formatter.Deserialize(readStream);
+
+
+
             Console.ReadKey();
         }
     }
---
+++ Console/Concepts/SerializationDeserialization/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Concepts.SerializationDeserialization
+{
+    [Serializable]
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
