73f7d93699309e85bd7d9f4387e31c11b32c1192
usman_solemn <usman.solemn@gmail.com>
2021-05-10 15:07:50 +0500

    Implementation of windows service with running in console in debug mode

---
+++ Background Services/WindowsService/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace WindowsService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+
+        static void Main(string[] args)
+        {
+            if (Environment.UserInteractive)
+            {
+                Service1 service1 = new Service1();
+                service1.TestStartupAndStop(args);
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                new Service1()
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+
+        }
+    }
+}
---
+++ Background Services/WindowsService/Service1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace WindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        internal void TestStartupAndStop(string[] args)
+        {
+            this.OnStart(args);
+            Console.ReadLine();
+            this.OnStop();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            EventLog eventLog = new EventLog();
+            eventLog.Source = "Service1";
+            eventLog.WriteEntry("Service1 started.", EventLogEntryType.Information);
+        }
+
+        protected override void OnStop()
+        {
+            EventLog eventLog = new EventLog();
+            eventLog.Source = "Service1";
+            eventLog.WriteEntry("Service1 stopped.", EventLogEntryType.Information);
+        }
+    }
+}
