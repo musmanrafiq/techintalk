19ab355b925620bbd0b6923f975a58f42200e1cf
usman_solemn <usman.solemn@gmail.com>
2021-12-06 18:52:15 +0500

    Csharp 10, Tips&Tricks - ArgumentNullException

---
+++ csharp10/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    private static string? name;
+
+    public static void Main(string[] args)
+    {
+        SayHi(name);
+    }
+
+    private static void SayHi(string name)
+    {
+        // before csharp 10
+        /*if (string.IsNullOrEmpty(name))
+        {
+            throw new ArgumentNullException(nameof(name));
+        }*/
+
+        // now in csharp 10
+        ArgumentNullException.ThrowIfNull(name, "name");
+
+        Console.WriteLine(name);    
+    }
+}
