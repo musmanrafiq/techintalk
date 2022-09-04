c5fe96e73c4c0d95c32463eace68a587535343d9
Usman <usman.solemn@gmail.com>
2022-09-04 12:50:08 +0500

    csharp11 practical example

---
+++ csharp11/ConsoleExample/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var Usman = "Usman Rafiq";
+
+    }
+
+    public static string ListPattern(int? input)
+    {
+
+        return string.Empty;
+    }
+}
+
