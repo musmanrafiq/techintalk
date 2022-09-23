dbe0f06b9857be702019be1830bf925e1160fe3c
Usman <usman.solemn@gmail.com>
2022-09-23 07:04:38 +0500

    In keyword example

---
+++ LanguageFeatures/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GuessHeight(10);
+
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static void GuessHeight(in int age)
+        {
+            // just uncomment the line no. 15 and check the megic
+            //age = 10;
+        }
+    }
+
+
+}
